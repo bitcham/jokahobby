fe89cdc361222892ca48471eec0fd400dd811e99
Chambit Oh <chambit.oh@gmail.com>
2026-02-13 18:44:20 +0200

    refactor: remove unused ModelMapper and JPAQueryFactory from AppConfig

--- src/main/java/com/jokahobby/infra/config/AppConfig.java
+++ src/main/java/com/jokahobby/infra/config/AppConfig.java
@@ -1,42 +1,9 @@
 package com.jokahobby.infra.config;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
-import org.modelmapper.AbstractConverter;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.NameTokenizers;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-
 @Configuration
-@RequiredArgsConstructor
 @EnableScheduling
 public class AppConfig {
-
-    private final EntityManager entityManager;
-
-    @Bean
-    public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setDestinationNameTokenizer(NameTokenizers.UNDERSCORE)
-                .setSourceNameTokenizer(NameTokenizers.UNDERSCORE);
-        modelMapper.addConverter(new AbstractConverter<LocalDateTime, Instant>() {
-            @Override
-            protected Instant convert(LocalDateTime source) {
-                return source == null ? null : source.toInstant(ZoneOffset.UTC);
-            }
-        });
-        return modelMapper;
-    }
-
-    @Bean
-    public JPAQueryFactory jpaQueryFactory() {
-        return new JPAQueryFactory(entityManager);
-    }
 }
