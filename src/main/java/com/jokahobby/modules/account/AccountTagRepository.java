8d4a3ef98b928640f9d62c18a1833e6e3fc26f2e
Chambit Oh <chambit.oh@gmail.com>
2026-02-15 21:33:38 +0200

    fix: resolve LazyInitializationException in account tags/zones queries

--- src/main/java/com/jokahobby/modules/account/AccountTagRepository.java
+++ src/main/java/com/jokahobby/modules/account/AccountTagRepository.java
@@ -2,6 +2,7 @@ package com.jokahobby.modules.account;
 
 import com.jokahobby.modules.tag.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.UUID;
 
 public interface AccountTagRepository extends JpaRepository<AccountTag, Long> {
 
+    @Query("SELECT at FROM AccountTag at JOIN FETCH at.tag WHERE at.account.id = :accountId")
     List<AccountTag> findAllByAccountId(UUID accountId);
 
     Optional<AccountTag> findByAccountAndTag(Account account, Tag tag);
--- src/main/java/com/jokahobby/modules/account/AccountZoneRepository.java
+++ src/main/java/com/jokahobby/modules/account/AccountZoneRepository.java
@@ -2,6 +2,7 @@ package com.jokahobby.modules.account;
 
 import com.jokahobby.modules.zone.Zone;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.UUID;
 
 public interface AccountZoneRepository extends JpaRepository<AccountZone, Long> {
 
+    @Query("SELECT az FROM AccountZone az JOIN FETCH az.zone WHERE az.account.id = :accountId")
     List<AccountZone> findAllByAccountId(UUID accountId);
 
     Optional<AccountZone> findByAccountAndZone(Account account, Zone zone);
